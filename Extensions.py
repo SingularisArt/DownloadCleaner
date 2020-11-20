0497891effdbe46e9020819a6d721be0866c77d9
Hashem A. Damrah <dermdorm2@gmail.com>
2020-11-20 01:02:03 -0800

    Added some files and removed some files

--- Extensions.py
+++
@@ -1,6 +0,0 @@
-PATH      = '/home/hashr/Downloads'
-MP4       = ['.mp4', '.3gp', '.0gg', '.webm', '.flv', '.avi', '.wmv']
-MP3       = ['.mp3', '.3ga', '.abc', '.flac', '.ac3', '.wav', '.aac']
-IMAGES    = ['.png', '.gif', '.jpg', '.jpeg', '.pdf', '.eps', '.bmp']
-DOCUMENTS = ['.doc', '.txt', '.odt', '.xlsx', '.rtf', '.tex', '.wpd']
-ZIP       = ['.zip', '.war', '.tar', '.lzma', '.iso', '.deb', '.exe']
